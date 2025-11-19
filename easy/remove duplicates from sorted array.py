e75dc02cfb4cebac183c55a064c73f035e3fd570
Imtela Islam <imtela@gmail.com>
2025-11-19 16:17:53 +0600

    added file

---
+++ easy/remove duplicates from sorted array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def removeDuplicates(self, nums) -> int:
+        k = 1
+        for i in range(1,len(nums)):
+            if nums[i] != nums[i-1]:
+                nums[k] = nums[i]
+                k += 1
+                
+        return f'{k}, nums = {nums}'
+sol = Solution()
+print(sol.removeDuplicates([0,0,1,1,1,2,2,2,3,3,4]))
