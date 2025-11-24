f01002be25027201d78ed60b6e54e24a3dc45580
Imtela Islam <imtela@gmail.com>
2025-11-24 19:47:35 +0600

    added file

---
+++ easy/plus one.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        if digits==[]:
+            return [1]
+        if digits[-1]!=9:
+            digits[-1]+=1
+            return digits
+        else:
+            plus = self.plusOne(digits[:-1])
+            plus.append(0)
+            return plus
+
+
+
+
+            
+        
+sol = Solution()
+print(sol.plusOne([9,9,9,9,9])) 
